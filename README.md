4530b57fd1cead7aa040eb5f503aefbfbb0ce12d
James Frost <git@frost.cx>
2023-03-19 12:47:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Monitoring service
+
+This is a program to monitor HTTP services and send notification emails when
+their status changes. It is intended to easily integrate with
+[Statuspage](https://statuspage.io), but is hopefully generally useful
+otherwise.
+
+## Requirements
+
+- [Python](https://www.python.org) 3.11 or newer.
+- [Requests](https://requests.readthedocs.io/)
+- An SMTP server to send with. I use [AWS SES](https://aws.amazon.com/ses/).
+
+## Usage
+
+You must first make a copy of `config.toml.example`, with the `.example` suffix
+removed, and change the contained configuration for your services.
+
+Once that is done you can run the tool with `python3 monitor.py`.
+
+Once you have confirmed it as working you will need to regularly run it to
+monitor your services. I would suggest putting an entry in your crontab to run
+it every 5 minutes. For example:
+
+```sh
+# Example crontab entry to run every 5 minutes.
+*/5 * * * * path/to/monitor.py
+```
---
+++ monitor.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+import dbm
+import datetime
+import tomllib
+import requests
+import send_email
+
+with open("config.toml", "rb") as fp:
+    config = tomllib.load(fp)
+
+# Unix key-value database thing. Interface is dictionary like, but it only
+# stores bytes.
+with dbm.open("status.dbm", "c") as db:
+    services = config["services"]
+    for service_name in services:
+        service = services[service_name]
+        r = requests.get(service["http_endpoint"])
+        if r.status_code == requests.codes.ok:
+            status = "UP"
+        else:
+            status = "DOWN"
+        try:
+            old_status = db[service_name].decode()
+        except KeyError:
+            status = None
+        if status != old_status:
+            # Status has changed. Update stored value and send notification email.
+            subject = f"{service_name} is {status}"
+            timestamp = (
+                datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()
+            )
+            body = f"""This is an automated monitoring email.
+
+            At {timestamp} the status of {service_name} changed to {status}.
+            It returned a status code of {r.status_code}.
+            """
+            send_email.send_email(service["email"], subject, body)
+            send_email.send_email(config["to_addresses"], subject, body)
+            db[service_name] = status
